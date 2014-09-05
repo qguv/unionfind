6a0be8ef0a42917bd978cbcab2ff293833e0e6dc
Quint Guvernator <quintus.public@gmail.com>
2014-09-04 22:58:48 -0400

    document and gofmt for godoc

--- qfarray/qfarray.go
+++ qfarray/qfarray.go
@@ -1,10 +1,19 @@
 package qfarray
 
+/* UnionFind contains information about how given numbers are related. Use
+UnionFind.Union(2, 3) to specify that 2 and 3 belong to the same set.
+Additional calls to Union can create relationships by the transitive
+property. Query the relationship between numbers with the Connected or Find
+methods.
+This implementation of UnionFind uses quick-find and stores connection data in
+a map.
+*/
 type UnionFind struct {
 	id    []int
 	count int
 }
 
+// New initializes a new UnionFind struct and returns a pointer.
 func New(count int) *UnionFind {
 	id := make([]int, count)
 	for i := 0; i < count; i++ {
@@ -13,14 +22,18 @@ func New(count int) *UnionFind {
 	return &UnionFind{id: id, count: count}
 }
 
+// Find returns the set ID of a given element.
 func (uf UnionFind) Find(p int) int {
 	return uf.id[p]
 }
 
+// Connected determines whether two elements belong to the same set.
 func (uf UnionFind) Connected(p, q int) bool {
 	return uf.id[p] == uf.id[q]
 }
 
+// Union specifies two elements as belonging to the same set for future query
+// with the Connected or Find methods.
 func (uf *UnionFind) Union(p, q int) {
 	if uf.Connected(p, q) {
 		return
--- qfmap/qfmap.go
+++ qfmap/qfmap.go
@@ -1,34 +1,47 @@
 package qfmap
 
+/* UnionFind contains information about how given numbers are related. Use
+UnionFind.Union(2, 3) to specify that 2 and 3 belong to the same set.
+Additional calls to Union can create relationships by the transitive
+property. Query the relationship between numbers with the Connected or Find
+methods.
+This implementation of UnionFind uses quick-find and stores connection data in
+a map.
+*/
 type UnionFind struct {
-	id    map[int]int
-	next	int
+	id   map[int]int
+	next int
 }
 
+// New initializes a new UnionFind struct and returns a pointer.
 func New() *UnionFind {
 	return &UnionFind{id: make(map[int]int)}
 }
 
+// Find returns the set ID of a given element.
 func (uf UnionFind) Find(p int) int {
 	return uf.id[p]
 }
 
+// Connected determines whether two elements belong to the same set.
 func (uf UnionFind) Connected(p, q int) bool {
-  p_id, p_in_map := uf.id[p]
-  q_id, q_in_map := uf.id[q]
-
-	if !(p_in_map && q_in_map) { return false }
-
+	p_id, p_in_map := uf.id[p]
+	q_id, q_in_map := uf.id[q]
+	if !(p_in_map && q_in_map) {
+		return false
+	}
 	return p_id == q_id
 }
 
+// Union specifies two elements as belonging to the same set for future query
+// with the Connected or Find methods.
 func (uf *UnionFind) Union(p, q int) {
 	if uf.Connected(p, q) {
 		return
 	}
 
-  p_id, p_in_map := uf.id[p]
-  q_id, q_in_map := uf.id[q]
+	p_id, p_in_map := uf.id[p]
+	q_id, q_in_map := uf.id[q]
 
 	var next_consumed bool
 
@@ -40,13 +53,15 @@ func (uf *UnionFind) Union(p, q int) {
 
 	if !q_in_map {
 		uf.id[q] = uf.next
-    q_id = uf.next
+		q_id = uf.next
 		next_consumed = true
 	}
-	
-	if next_consumed { uf.next++ }
-	
-  for i, n := range uf.id {
+
+	if next_consumed {
+		uf.next++
+	}
+
+	for i, n := range uf.id {
 		if n == p_id {
 			uf.id[i] = q_id
 		}
